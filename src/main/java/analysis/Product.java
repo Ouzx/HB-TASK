a4a8f6a1984285e9e15f6d0ff925a4d196908d40
Oğuzhan Kandakoğlu <47363718+Ouzx@users.noreply.github.com>
2021-08-09 00:28:27 +0300

    pre analysis2

---
+++ src/main/java/analysis/Product.java
@@ -0,0 +1,14 @@
+package analysis;
+
+public class Product {
+
+    public Product(String productName, String seller, String category) {
+        this.productName = productName;
+        this.seller = seller;
+        this.category = category;
+    }
+
+    public String productName;
+    public String category;
+    public String seller;
+}
